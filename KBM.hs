ae94af6f960df48b4f9436b3aef347f8a0e10652
David McGillicuddy <dmcgill9071@gmail.com>
2013-10-17 15:09:26 +0100

    removed some pointless imports. Not tested!

--- KBM.hs
+++ KBM.hs
@@ -4,10 +4,7 @@ import Control.Lens
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
-import Game (Pos(..))
 import State
-import Helpers
-import Draw
 
 handleInput :: Event -> State -> State
 handleInput (EventKey k keyState _ _) = keysDown . contains k .~ (keyState == Down)
